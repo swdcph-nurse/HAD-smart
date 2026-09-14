e32f23d1a09fda97aa77e591b819ccf5eb8d70af
swdcph.nurse <parichat4.vip@gmail.com>
2026-09-14 16:56:32 +0700

    feat: activate role-aware medication kiosk

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,6 +5,7 @@ import { AppShell } from '@/components/layout/AppShell'
 import { LoginPage } from '@/pages/LoginPage'
 import { LandingPage } from '@/pages/LandingPage'
 import { NurseRolePage } from '@/pages/NurseRolePage'
+import { MedicationKioskPage } from '@/pages/MedicationKioskPage'
 import { HomePage } from '@/pages/HomePage'
 import { ProfilePage } from '@/pages/ProfilePage'
 import { NotFoundPage } from '@/pages/NotFoundPage'
@@ -15,13 +16,12 @@ import { AlertsPage } from '@/pages/alerts/AlertsPage'
 import { AssessmentPage } from '@/pages/assessment/AssessmentPage'
 import { RoleGate } from '@/components/layout/RoleGate'
 import { KioskPage } from '@/pages/KioskPage'
-
 export default function App() {
   return <BrowserRouter><AuthProvider><Routes>
     <Route path="/" element={<LandingPage />} />
     <Route path="/nurse" element={<NurseRolePage />} />
-    <Route path="/nurse/med" element={<KioskPage />} />
-    <Route path="/nurse/charge" element={<KioskPage />} />
+    <Route path="/nurse/med" element={<MedicationKioskPage />} />
+    <Route path="/nurse/charge" element={<MedicationKioskPage />} />
     <Route path="/kiosk" element={<KioskPage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route element={<ProtectedRoute />}><Route element={<AppShell />}>
